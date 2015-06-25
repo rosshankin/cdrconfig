809887eb33c9a38ada4eecf0ef62528dd8a3c87d
Ross <rosshankin@gmail.com>
2015-06-25 15:41:34 -0400

    wip

--- db_gui.py
+++ db_gui.py
@@ -1,6 +1,8 @@
 from Tkinter import *
+import tkMessageBox
 import db_pbx_target
 import db_pbx_misconfig
+import db_range_target
 
 class Database(Frame):
 
@@ -10,6 +12,8 @@ class Database(Frame):
         self.grid()
         self.pbx_map = {}
         self.pbx_map2 = {}
+        self.ids = {}
+        self.engines = ''
 
     def create_widgets(self):
         self.ent_ip = Entry(self, width = 19)
@@ -32,19 +36,19 @@ class Database(Frame):
         self.bttn_add.grid(row = 8, column = 1, sticky = W)
         self.bttn_last = Button(self,text="Last Entry",command = self.last)
         self.bttn_last.grid(row=2, column = 1)
-        self.lbl_key = Label(self,text = 'Key').grid(row = 9,column = 1,columnspan = 2)
+        self.lbl_key = Label(self,text = 'Key').grid(row = 9,column = 1)
         self.lb_key = Listbox(self,selectmode = SINGLE,exportselection = False)
-        self.lb_key.grid(row = 10, column = 1, columnspan = 2)
+        self.lb_key.grid(row = 10, column = 1, columnspan = 2, sticky = W)
         self.lb_key.bind("<Double-Button-1>",self.key_select)
         self.lbl_value = Label(self, text = 'Value').grid(row = 9, column = 3,columnspan = 2)
         self.lb_value = Listbox(self,selectmode = SINGLE, exportselection = False)
-        self.lb_value.grid(row = 10, column = 3, columnspan = 2)
+        self.lb_value.grid(row = 10, column = 3, columnspan = 2,sticky = W)
         self.lb_value.bind("<Double-Button-1>", self.value_select)
-        self.ent_key = Entry(self,width = 22)
+        self.ent_key = Entry(self,width = 19)
         self.ent_key.config(state=NORMAL)
         self.ent_key.grid(row = 11, column = 1, columnspan = 3, sticky = W)
-        self.ent_value = Entry(self,width = 20)
-        self.ent_value.grid(row = 11, column = 3, columnspan = 3)
+        self.ent_value = Entry(self,width = 19)
+        self.ent_value.grid(row = 11, column = 3, columnspan = 3, sticky = W)
         self.bttn_reset = Button(self, text = 'Reset', command = self.reset)
         self.bttn_reset.grid(row = 1, column = 3, sticky = W)
         self.lbl_success = Label(self, text = '', fg = 'green')
@@ -53,6 +57,8 @@ class Database(Frame):
         self.bttn_add.grid(row = 12, column = 1, sticky = W)
         self.lbl_status = Label(self,text = '')
         self.lbl_status.grid(row = 8, column = 1, columnspan = 2, sticky = E)
+        self.bttn_window = Button(self, text = 'sf_range/extension', command = self.window_box)
+        self.bttn_window.grid(row = 3, column = 2, columnspan = 2)
         
     ## get ip address
     ## run and connect engine
@@ -66,6 +72,7 @@ class Database(Frame):
             if not ip == "":
                 f.write(ip)
             engine_name = 'postgresql+pg8000://postgres:molly36@' + ip + '/ross'
+            self.engines += engine_name
             import sqlalchemy as sqla
             engine = sqla.create_engine(engine_name)
             try:
@@ -75,10 +82,11 @@ class Database(Frame):
                 self.ent_ip.insert(0,'Invalid')
                 return
             #sf_pbx
-            result = conn.execute('select server_name,enable_cdr,cdr_port,cdr_zone from sf_pbx')
+            result = conn.execute('select server_name,enable_cdr,cdr_port,cdr_zone, id from sf_pbx')
             for row in result:
                 self.lb.insert(END,row[0])
-                self.pbx_map[row[0]] = db_pbx_target.PBX(row[0],row[1],row[2],row[3])
+                self.pbx_map[row[0]] = db_pbx_target.PBX(row[0],row[1],row[2],row[3],row[4])
+                self.ids[row[0]] = db_range_target.Range(row[0],row[4])
             self.lb.selection_set(0)
             self.select(0)
         else:
@@ -97,6 +105,7 @@ class Database(Frame):
         t = self.lb.curselection()
         index = t[0]
         text = self.lb.get(index)
+        nameid = self.ids[text].name_id
         zone = self.pbx_map[text].zone
         self.ent_zone.insert(0,zone)
         port = self.pbx_map[text].port
@@ -258,11 +267,130 @@ class Database(Frame):
         self.ent_1.delete(0,END)
         self.ent_2.delete(0,END)
         self.ent_3.delete(0,END)
-               
+        
+    ## create dialog box for access to ranges and extensions
+    ## passes information from Database class into Window class
+    def window_box(self):
+        t = self.lb.curselection()
+        if t:
+            index = t[0]
+            text = self.lb.get(index)
+            nameid = self.ids[text].name_id
+            win = Toplevel()
+            box = Window(win,nameid,self.engines)
+            box.pack()
+            win.grab_set()
+            win.focus_set()
+            win.wait_window()
 
+class Window(Frame):
+    
+    def __init__(self,win,ids,engines):
+        Frame.__init__(self,win)
+        self.win = win
+        self.ids = ids
+        self.engines = engines
+        self.create_widgets()
+        self.range_targets = {}
+        self.extensions= {}
+
+    def create_widgets(self):
+        self.bttn_load = Button(self,text = 'Load', command = self.load)
+        self.bttn_load.grid(row = 1,column = 1)
+        self.bttn_ext = Button(self,text = 'Load Extensions',command = self.load_ext)
+        self.bttn_ext.grid(row = 2, column = 1)
+        self.lb_range = Listbox(self,selectmode=SINGLE,exportselection = False)
+        self.lb_range.grid(row = 3, column = 1)
+        self.bttn_add_range = Button(self, text = 'Add New Range', command = self.add_range)
+        self.bttn_add_range.grid(row = 6, column = 1)
+        self.bttn_delete = Button(self,text = 'Delete Range', command = self.delete_range)
+        self.bttn_delete.grid(row = 7,column = 1)
+        self.lbl_range_lower = Label(self,text = 'Lower:').grid(row =4,column = 1, sticky = W)
+        self.ent_range_lower = Entry(self, width = 10)
+        self.ent_range_lower.grid(row = 4, column = 1, columnspan = 2, sticky = E)
+        self.lbl_range_upper = Label(self,text = 'Upper:').grid(row = 5, column = 1, sticky = W)
+        self.ent_range_upper = Entry(self, width = 10)
+        self.ent_range_upper.grid(row =5, column = 1, columnspan = 2, sticky = E)
+        self.bttn_term = Button(self,text = 'Exit', command = self.terminate)
+        self.bttn_term.grid(row= 9, column = 1)
+        
+    ## inserts all ranges into listbox
+    def load(self):
+        self.lb_range.delete(0,END)
+        try:
+            import sqlalchemy as sqla
+            engine = sqla.create_engine(self.engines)
+            conn = engine.connect()
+            result = conn.execute("select id, lowerbound, upperbound from sf_range")
+            for row in result:
+                ids = str(row[0])
+                lower = str(row[1])
+                upper = str(row[2])
+                range_string = ids, lower, upper
+                self.lb_range.insert(END,range_string)
+        except:
+            self.error()
+            self.win.destroy()
+            
+    ## returns to db_gui.py if error occurs loading ranges
+    def error(self):
+        import tkMessageBox
+        tkMessageBox.showinfo("Error", "Restart Program")
+        
+    ## ability to add a range into sf_range
+    def add_range(self):
+        lower = self.ent_range_lower.get()
+        upper = self.ent_range_upper.get()
+        import sqlalchemy as sqla
+        engine = sqla.create_engine(self.engines)
+        conn = engine.connect()
+        if self.ent_range_lower.get() != '' and self.ent_range_upper.get() != '':
+            conn.execute("insert into sf_range values (DEFAULT, '" + lower.rstrip() + "','" + upper.rstrip() + "')");
+            self.ent_range_lower.delete(0,END)
+            self.ent_range_upper.delete(0,END)
+
+    ## ability to delete single range from listbox and sf_range
+    def delete_range(self):
+        import sqlalchemy as sqla
+        engine = sqla.create_engine(self.engines)
+        conn = engine.connect()
+        t = self.lb_range.curselection()
+        index = t[0]
+        text = self.lb_range.get(index)
+        rangeid = text[0]
+        result = conn.execute("delete from sf_range where sf_range.id = '" + rangeid + "'")
+        self.lb_range.delete(t)
+        
+    ## load and delete extensions in sf_extension relative to selected range
+    def load_ext(self):
+        t = self.lb_range.curselection()
+        if t:
+            index = t[0]
+            text = self.lb_range.get(index)
+            import sqlalchemy as sqla
+            engine = sqla.create_engine(self.engines)
+            conn = engine.connect()
+            pbx_id = self.ids
+            lower = int(text[1])
+            upper = int(text[2]) + 1
+            print lower, upper
+            for ext in range(lower, upper):
+                exten = "insert into sf_extension values(DEFAULT,'" + str(ext) + "', false," + str(text[0]) + ",DEFAULT, NULL, NULL," + str(pbx_id) + ")"
+                try:
+                 
+                    result3 = conn.execute(exten);
+                except:
+                    pass
+
+    ## exit the dialog box
+    def terminate(self):
+        import tkMessageBox
+        if tkMessageBox.askokcancel('Exit','Return to Program?'):
+            self.win.destroy()
+
+#main            
 root = Tk()
 root.title('Database GUI')
 root.resizable(width = True, height = True)
 db = Database(root)
-root.mainloop()
-                            
+root.mainloop()                           
--- db_pbx_target.py
+++ db_pbx_target.py
@@ -1,10 +1,11 @@
 
 class PBX(object):
-    def __init__(self,server_name,enable,port,zone):
+    def __init__(self, server_name,enable,port,zone,ids):
         self.server_name = server_name
         self.enable = enable
         self.port = int(port)
         self.zone = zone
+        self.ids = ids
 
    
         
